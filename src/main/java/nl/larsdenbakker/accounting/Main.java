a936356e58733ad77df8aa0b2ff00f996502857b
Lars den Bakker <larsdenbakker@gmail.com>
2015-10-30 10:55:00 +0100

    Tweak Main.

--- src/main/java/nl/larsdenbakker/accounting/Main.java
+++ src/main/java/nl/larsdenbakker/accounting/Main.java
@@ -2,25 +2,25 @@ package nl.larsdenbakker.accounting;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import nl.larsdenbakker.app.UserInputException;
 
 /**
+ * TODO: Need to do more research into how the JavaFX Application.launch() works.
+ * It needs to be separate from Main.
  *
  * @author Lars den Bakker <larsdenbakker at gmail.com>
  */
-public class Main extends Application {
+public class Main extends javafx.application.Application {
 
    private static AccountingApplication app;
-   private static Main inst;
 
    public static void main(String[] args) {
       try {
          app = new AccountingApplication();
          app.load();
-         Application.launch(args);
+         javafx.application.Application.launch(args);
       } catch (Exception ex) {
          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "An exception occurred during exception", ex);
          Platform.exit();
@@ -29,7 +29,6 @@ public class Main extends Application {
 
    @Override
    public void start(Stage primaryStage) throws Exception {
-      inst = this;
       app.getRegistryGUIModule().addToStage(primaryStage);
    }
 
